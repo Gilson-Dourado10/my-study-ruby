72ccfcb31685bdc606c2d0f539bd89dc92c128b2
gilson batista de almeida dourado <gilsondourado10@gmail.com>
2022-03-24 06:56:29 -0300

    array

---
+++ treino.rb
@@ -0,0 +1,14 @@
+
+array_cidades = [ " Belo Horizonte ", "Rio de Janeiro ", "São Paulo"," Salvador" , "Salvador", "Santo Andre" , "Mauá"]
+array_cidades = [0]
+array_cidades = [1]
+array_cidades = [2]
+array_cidades = [3]
+array_cidades = [4]
+array_cidades = [5]
+array_cidades << ["Brasília"]
+array_cidades[5] = "OLinda"
+array_cidades + ["Fortaleza"]
+puts array_cidades
+
+
