406475c0818f179f57c2c5d623d2285ef48e8249
gilson batista de almeida dourado <gilsondourado10@gmail.com>
2022-03-24 21:54:30 -0300

    arrays

--- treino.rb
+++ treino.rb
@@ -1,14 +1,24 @@
 
 array_cidades = [ " Belo Horizonte ", "Rio de Janeiro ", "São Paulo"," Salvador" , "Salvador", "Santo Andre" , "Mauá"]
-array_cidades = [0]
-array_cidades = [1]
-array_cidades = [2]
-array_cidades = [3]
-array_cidades = [4]
-array_cidades = [5]
+array_cidades[0]
+array_cidades[1]
+array_cidades[2]
+array_cidades[3]
+array_cidades[4]
+array_cidades[5]
 array_cidades << ["Brasília"]
 array_cidades[5] = "OLinda"
 array_cidades + ["Fortaleza"]
 puts array_cidades
+puts array_cidades[0]
+p array_cidades[1]
+p array_cidades[2]
+p array_cidades[3]
+p array_cidades[4]
 
-
+nomes =         [' GILSON DOURADO', 'JONATHAN' , 'MESSIAS']
+p nomes
+p nomes[0]
+p nomes[1]
+p nomes[2]
+p nomes [3]
