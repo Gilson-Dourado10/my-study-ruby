84acdff429f2d9e24699933dce127cca0ebd135c
gilson batista de almeida dourado <gilsondourado10@gmail.com>
2022-03-25 23:04:08 -0300

    metodos arrays

--- treino.rb
+++ treino.rb
@@ -1,24 +1,57 @@
 
-array_cidades = [ " Belo Horizonte ", "Rio de Janeiro ", "São Paulo"," Salvador" , "Salvador", "Santo Andre" , "Mauá"]
-array_cidades[0]
-array_cidades[1]
-array_cidades[2]
-array_cidades[3]
-array_cidades[4]
-array_cidades[5]
-array_cidades << ["Brasília"]
-array_cidades[5] = "OLinda"
-array_cidades + ["Fortaleza"]
-puts array_cidades
-puts array_cidades[0]
-p array_cidades[1]
-p array_cidades[2]
-p array_cidades[3]
-p array_cidades[4]
+ary = [1, "two", 3.0] #=> [1, "two", 3.0]
+ary  = Array.new    #=> []
+Array.new(3)       #=> [nil, nil, nil]
+Array.new(3, true) #=> [true, true, true]
 
-nomes =         [' GILSON DOURADO', 'JONATHAN' , 'MESSIAS']
-p nomes
-p nomes[0]
-p nomes[1]
-p nomes[2]
-p nomes [3]
+Array.new(4) {Hash.new}    #=> [{}, {}, {}, {}]
+Array.new(4) {|i| i.to_s } #=> ["0", "1", "2", "3"]
+
+empty_table = Array.new(3) {Array.new(3)}
+#=> [[nil, nil, nil], [nil, nil, nil], [nil, nil, nil]]
+
+Array({:a => "a", :b => "b"}) #=> [[:a, "a"], [:b, "b"]]
+
+
+
+arr = [1, 2, 3, 4, 5, 6]
+arr[2]    #=> 3
+arr[100]  #=> nil
+arr[-3]   #=> 4
+arr[2, 3] #=> [3, 4, 5]
+arr[1..4] #=> [2, 3, 4, 5]
+arr[1..-3] #=> [2, 3, 4]
+
+arr = [ 1, 2, 3, 4, 5, 6 ]
+arr[2]     #=> 3
+arr [100] #> nil
+arr[-3]   #=> 4
+arr[2,3]    #> [2,3,4]
+arr[1..4]   #=>
+arr[1..-3]
+
+arr = ['a', 'b', 'c', 'd', 'e', 'f']
+arr.fetch(100) #=> IndexError: index 100 outside of array bounds: -6...6
+arr.fetch(100, "oops") #=> "oops"
+
+
+arr.first #=> 1
+arr.last  #=> 6
+arr.take(3) #=> [1, 2, 3]
+arr.drop(3) #=> [4, 5, 6]
+
+
+browsers = ['Chrome', 'Firefox', 'Safari', 'Opera', 'IE']
+browsers.length #=> 5
+browsers.count #=> 5
+browsers.empty? #=> false
+
+arr = [1, 2, 3, 4]
+arr.push(5) #=> [1, 2, 3, 4, 5]
+arr << 6    #=> [1, 2, 3, 4, 5, 6]
+
+arr.unshift(0) #=> [0, 1, 2, 3, 4, 5, 6]
+arr.insert(3, 'apple')  #=> [0, 1, 2, 'apple', 3, 4, 5, 6]
+
+arr.insert(3, 'orange', 'pear', 'grapefruit')
+#=> [0, 1, 2, "orange", "pear", "grapefruit", "apple", 3, 4, 5, 6]
