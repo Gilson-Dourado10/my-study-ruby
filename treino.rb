cbba9d2df66a7045a8c8dadde9915b7ff997a161
gilson batista de almeida dourado <gilsondourado10@gmail.com>
2022-03-26 23:55:06 -0300

    loops

--- treino.rb
+++ treino.rb
@@ -1,57 +1,65 @@
+$i = 0
+$num = 5
+begin
+  puts("Inside the loop i = #$i" )
+  $i +=1
 
-ary = [1, "two", 3.0] #=> [1, "two", 3.0]
-ary  = Array.new    #=> []
-Array.new(3)       #=> [nil, nil, nil]
-Array.new(3, true) #=> [true, true, true]
-
-Array.new(4) {Hash.new}    #=> [{}, {}, {}, {}]
-Array.new(4) {|i| i.to_s } #=> ["0", "1", "2", "3"]
-
-empty_table = Array.new(3) {Array.new(3)}
-#=> [[nil, nil, nil], [nil, nil, nil], [nil, nil, nil]]
-
-Array({:a => "a", :b => "b"}) #=> [[:a, "a"], [:b, "b"]]
+  end while $i < $num
 
 
 
-arr = [1, 2, 3, 4, 5, 6]
-arr[2]    #=> 3
-arr[100]  #=> nil
-arr[-3]   #=> 4
-arr[2, 3] #=> [3, 4, 5]
-arr[1..4] #=> [2, 3, 4, 5]
-arr[1..-3] #=> [2, 3, 4]
+$i = 3
+$num = 6
+begin
+  puts ("está dentro do loop= #$i ")
 
-arr = [ 1, 2, 3, 4, 5, 6 ]
-arr[2]     #=> 3
-arr [100] #> nil
-arr[-3]   #=> 4
-arr[2,3]    #> [2,3,4]
-arr[1..4]   #=>
-arr[1..-3]
-
-arr = ['a', 'b', 'c', 'd', 'e', 'f']
-arr.fetch(100) #=> IndexError: index 100 outside of array bounds: -6...6
-arr.fetch(100, "oops") #=> "oops"
+  $i += 1
+  end while $i < $num
 
 
-arr.first #=> 1
-arr.last  #=> 6
-arr.take(3) #=> [1, 2, 3]
-arr.drop(3) #=> [4, 5, 6]
+$i = 0
+$num = 5
+begin
+  puts("Inside the loop i = #$i" )
+  $i +=1
+end until $i > $num
+
+$i = 0
+$num = 7
+
+begin
+  puts ( "está dentro do loop i= #$i")
+  $i += 1
+
+end until $i > $num
 
 
-browsers = ['Chrome', 'Firefox', 'Safari', 'Opera', 'IE']
-browsers.length #=> 5
-browsers.count #=> 5
-browsers.empty? #=> false
+for i in 0..5
+  puts "Value of local variable is #{i}"
+end
 
-arr = [1, 2, 3, 4]
-arr.push(5) #=> [1, 2, 3, 4, 5]
-arr << 6    #=> [1, 2, 3, 4, 5, 6]
+for i in 0..5
+  puts "o valor da varaiavel local é #{i}    "
+end
 
-arr.unshift(0) #=> [0, 1, 2, 3, 4, 5, 6]
-arr.insert(3, 'apple')  #=> [0, 1, 2, 'apple', 3, 4, 5, 6]
+for i in 0..6
+  puts " o valor da variável local é #{i}"
+end
 
-arr.insert(3, 'orange', 'pear', 'grapefruit')
-#=> [0, 1, 2, "orange", "pear", "grapefruit", "apple", 3, 4, 5, 6]
+
+(0..5).each do |i|
+  puts "Value of local variable is #{i}"
+end
+
+(0..5).each do |i|
+  puts "o valor da variavel é  #{i}"
+end
+
+(0..8).each do |i|
+  puts "a varaiável é #{i} "
+end
+
+
+
+
+
