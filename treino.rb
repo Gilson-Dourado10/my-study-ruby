e314e70878bb4bbce0b81f1895b5368b65d4cf18
gilson batista de almeida dourado <gilsondourado10@gmail.com>
2022-03-28 05:57:33 -0300

    times

--- treino.rb
+++ treino.rb
@@ -1,63 +1,18 @@
-$i = 0
-$num = 5
-begin
-  puts("Inside the loop i = #$i" )
-  $i +=1
+3.times do
+  print ' Gison bem vindo ao ruby'
+end
+2.times do
+  print 'Gilson aprendendo ruby '
+end
+puts " 2+6 "
 
-  end while $i < $num
-
-
-
-$i = 3
-$num = 6
-begin
-  puts ("está dentro do loop= #$i ")
-
-  $i += 1
-  end while $i < $num
-
-
-$i = 0
-$num = 5
-begin
-  puts("Inside the loop i = #$i" )
-  $i +=1
-end until $i > $num
-
-$i = 0
-$num = 7
-
-begin
-  puts ( "está dentro do loop i= #$i")
-  $i += 1
-
-end until $i > $num
-
-
-for i in 0..5
-  puts "Value of local variable is #{i}"
+2.times do
+  p ' disciplina e foco '
 end
 
-for i in 0..5
-  puts "o valor da varaiavel local é #{i}    "
-end
-
-for i in 0..6
-  puts " o valor da variável local é #{i}"
-end
+puts " 3+3 "
 
 
-(0..5).each do |i|
-  puts "Value of local variable is #{i}"
-end
-
-(0..5).each do |i|
-  puts "o valor da variavel é  #{i}"
-end
-
-(0..8).each do |i|
-  puts "a varaiável é #{i} "
-end
 
 
 
