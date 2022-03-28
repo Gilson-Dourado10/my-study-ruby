a62837f7da55cf97d851e167d276f4b3b80c1376
gilson batista de almeida dourado <gilsondourado10@gmail.com>
2022-03-28 06:28:38 -0300

    reverse, lenght

--- treino.rb
+++ treino.rb
@@ -1,16 +1,20 @@
-3.times do
-  print ' Gison bem vindo ao ruby'
-end
-2.times do
-  print 'Gilson aprendendo ruby '
-end
-puts " 2+6 "
+puts  3+3
+p 10*20
+print 5 + 5
+print 10+10
+puts  20/ 5
 
-2.times do
-  p ' disciplina e foco '
-end
+puts "gilson".reverse
+p 'gilson'.length
+print 'gilson'.length
+print 'dourado'.reverse
+puts " gilson \n" * 3
+puts "gilson\n"* 4
+puts "40".reverse
+puts '40'.length
 
-puts " 3+3 "
+
+
 
 
 
