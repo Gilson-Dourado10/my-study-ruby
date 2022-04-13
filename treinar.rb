62734322a3de8e9772e1aadfbefe3606e64f7509
gilson dourado <gilsondourado10@gmail.com>
2022-04-13 06:28:58 -0300

    '

--- treinar.rb
+++ treinar.rb
@@ -7,10 +7,15 @@ puts str.rjust  larguraDaLinha
 puts str.ljust larguraDaLinha
 puts str.rjust larguraDaLinha = 60
 puts str.center larguraDaLinha
+puts ' '
+larguraDaLinha = 50
+pedidoAumento= 'Chefe eu quero um aumento'
+chefeZangado = 'O QUE VC QUER DIZER COM "EU QUERO UM AUMENTO.!?" VOCÊ ESTÁ DESPEDIDO!!!'
 
-
-
-
+puts pedidoAumento.center larguraDaLinha
+puts chefeZangado.ljust larguraDaLinha
+puts pedidoAumento = 'chefe não faz isso eu preciso do emprego'
+puts chefeZangado = 'JA FALEI RUA AGORAAAAAAAAA!!???'
 
 
 
