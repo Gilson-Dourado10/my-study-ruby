32ca1da820d6f7961252f3223e9d4952857c907a
gilson dourado <gilsondourado10@gmail.com>
2022-04-13 05:43:43 -0300

    metodo rjust e ljust

--- treinar.rb
+++ treinar.rb
@@ -1,19 +1,15 @@
 
-larguraDaLinha = 50
-puts(                'Old Mother Hubbard'.center(larguraDaLinha))
-puts(               'Sat in her cupboard'.center(larguraDaLinha))
-puts(         'Eating her curds an whey,'.center(larguraDaLinha))
-puts(          'When along came a spider'.center(larguraDaLinha))
-puts(         'Which sat down beside her'.center(larguraDaLinha))
-puts('And scared her poor shoe dog away.'.center(larguraDaLinha))
+larguraDaLinha = 40
+str = '--> MENTORIA SPDV1 <--'
+puts str.ljust  larguraDaLinha
+puts str.center larguraDaLinha
+puts str.rjust  larguraDaLinha
+puts str.ljust larguraDaLinha
+puts str.rjust larguraDaLinha = 60
+puts str.center larguraDaLinha
 
-larguraDaLinha = 10
-puts(                'VELHA mãe América'.center(larguraDaLinha))
-puts ('           SENTOU-SE em seu banco'.center(larguraDaLinha))
-puts ('     PARA comer sua coalhadada e  soro de leite'.center(larguraDaLinha))
-puts('           QUANDO veio uma aranha '.center(larguraDaLinha))
-puts ('  QUE sentou ao lado dela '.center(larguraDaLinha))
-puts ('E assustou seu pobre cachorro SARDINHA'.center(larguraDaLinha))
+
+
 
 
 
