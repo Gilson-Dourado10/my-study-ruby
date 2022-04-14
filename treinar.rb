76422f6e3779f7602356d8dc0579183efc356af6
gilson dourado <gilsondourado10@gmail.com>
2022-04-14 06:12:30 -0300

    metodo rand

--- treinar.rb
+++ treinar.rb
@@ -1,25 +1,22 @@
 
-larguraDaLinha = 40
-str = '--> MENTORIA SPDV1 <--'
-puts str.ljust  larguraDaLinha
-puts str.center larguraDaLinha
-puts str.rjust  larguraDaLinha
-puts str.ljust larguraDaLinha
-puts str.rjust larguraDaLinha = 60
-puts str.center larguraDaLinha
-puts ' '
-larguraDaLinha = 50
-pedidoAumento= 'Chefe eu quero um aumento'
-chefeZangado = 'O QUE VC QUER DIZER COM "EU QUERO UM AUMENTO.!?" VOCÊ ESTÁ DESPEDIDO!!!'
+puts rand
+puts rand
+puts rand
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts(rand(6))
+puts(rand(8))
+puts(rand(4))
+puts(rand(99999999999999999999999999999999999999999999999999999999999))
+puts('O homem do tempo disse que existe '+rand(101).to_s+'% de chance de chover,')
+puts('mas você não pode nunca confiar num homem do tempo.')
 
-puts pedidoAumento.center larguraDaLinha
-puts chefeZangado.ljust larguraDaLinha
-puts pedidoAumento = 'chefe não faz isso eu preciso do emprego'
-puts chefeZangado = 'JA FALEI RUA AGORAAAAAAAAA!!???'
-
-
-
-
-
-
-
+p rand
+p rand
+p rand
+rand
+puts rand(10)
+p rand 9
+puts rand 0
+p('Gilson você tem '+rand(101).to_s+' % de chance de passar na prova de  matemática')
