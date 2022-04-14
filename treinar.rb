1e5c5c6c12b38c3347b433e70ae022e6eb87982e
gilson dourado <gilsondourado10@gmail.com>
2022-04-14 06:39:09 -0300

    método srand

--- treinar.rb
+++ treinar.rb
@@ -1,22 +1,22 @@
 
-puts rand
-puts rand
-puts rand
+srand 17
 puts(rand(100))
 puts(rand(100))
 puts(rand(100))
-puts(rand(6))
-puts(rand(8))
-puts(rand(4))
-puts(rand(99999999999999999999999999999999999999999999999999999999999))
-puts('O homem do tempo disse que existe '+rand(101).to_s+'% de chance de chover,')
-puts('mas você não pode nunca confiar num homem do tempo.')
+puts(rand(100))
+puts(rand(100))
+puts   'repetindo os números aleatórios'
+srand 17
+p rand 100
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
 
-p rand
-p rand
-p rand
-rand
-puts rand(10)
-p rand 9
-puts rand 0
-p('Gilson você tem '+rand(101).to_s+' % de chance de passar na prova de  matemática')
+srand 0
+puts rand 5
+puts rand 5
+p "  "
+srand 0
+p rand 5
+p rand 5
