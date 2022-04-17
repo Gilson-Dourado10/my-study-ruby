bf35ce86672ebde300a7f1734e32ff415cf76752
gilson dourado <gilsondourado10@gmail.com>
2022-04-17 06:59:23 -0300

    loop DO

--- treinar.rb
+++ treinar.rb
@@ -1,17 +1,34 @@
 # repetição looping while
 
-x = 10
-while x >= 1
-  puts 'RODANDO O LOOP  WHILE'
-  x -= 1
-end
 
-puts ' '
-puts ' '
-# looping for
 
-i = 'RODANDO LOOP FOR'
-for a  in 1..10 do
+#looping DO
+i = 2
+loop do
+  i = i + 1
   puts i
+  break         # this will cause execution to exit the loop
 end
 
+loop do
+  puts "Do you want to do that again?"
+  answer = gets.chomp
+  if answer != 'Y'
+    break
+  end
+end
+
+begin
+  puts "Do you want to do that again?"
+  answer = gets.chomp
+end while answer == 'Y'
+
+
+i = 9
+loop do
+  i = i + 1
+  puts i
+  if i  == 10
+  break
+  end
+end
