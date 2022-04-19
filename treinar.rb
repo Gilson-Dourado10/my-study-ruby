1e4887b6772d3138c7967bfec08ea496a8bcd3d2
gilson dourado <gilsondourado10@gmail.com>
2022-04-19 05:43:36 -0300

    operador OR

--- treinar.rb
+++ treinar.rb
@@ -1,33 +1,20 @@
 
-i = 0
-loop do
-   i += 1
-  if i  == 4
-    next
+puts 'ola, qual o seu nome?'
+nome = gets.chomp
+puts ' ola,' +nome+ ' .'
+if nome == 'Gil'
+  puts ' que nome bonito '
+else
+  if nome == 'Jade'
+    puts 'que nome bonito!'
   end
-   puts i
-   if i == 10
-     break
-   end
 end
-p'****************************************'
-x = 42
-loop do
-  puts x
-  x = 2
-  break
-end
-puts x
-p'======================================'
-x =  gets.chomp.to_i
-until x < 0
-  puts x
-  x -= 1
-end
-puts 'done'
-p'****************************'
-i = 0
-until i == 5
-  puts i
-  i += 1
+
+p '======================================='
+
+puts 'Olá, qual o seu nome?'
+nome = gets.chomp
+puts 'ola ' + nome + ' . '
+if (nome == ' Gil ' or nome == ' Jade ')
+  puts ' Que nome bonito!!!'
 end
