43929c6c0d30cc34701e574f917abf44d4c7995f
gilson dourado <gilsondourado10@gmail.com>
2022-04-21 06:40:15 -0300

    each e times

--- treinar.rb
+++ treinar.rb
@@ -1,24 +1,14 @@
 
-puts []
-puts [5]
-puts ['frango '+ ' peixe']
-puts sabor = ' baunilha'
-nomes = ['ana' , 'Gilson' ,' jose' ]
-puts [89.9, sabor , [true, false], nomes ]
-puts '*************************************************'
- nomes = ['ana' , 'Gilson' ,' jose' ]
-puts nomes
-puts nomes [0]
-puts nomes [1]
-puts nomes [2]
-p nomes [3]
-puts nomes [3]
-p'========================================'
 
-linguagens = ['Matemática ', ' Ruby', ' Português' ]
-linguagens.each do |ling|
-  puts 'eu adoro' + ling+ ' !'
-  puts 'e você? '
+
+pessoa = ['MARIA' , 'VITORIA' , 'TERESA']
+pessoa.each do  |iterar|
+  puts 'Eu gosto da ' + iterar + '!!!'
+  puts 'e você ?'
 end
-puts 'E vamos ouví-lo sobre C++!'
-puts '...'
+puts ' do restante da família não!!!'
+p'???????'
+p'================================================'
+3.times do
+puts 'bom dia no foco nos estudos'
+end
