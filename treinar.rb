c6444adfe5c121e1021170aa13a8a912b557927a
gilson dourado <gilsondourado10@gmail.com>
2022-04-21 07:14:23 -0300

    join e times

--- treinar.rb
+++ treinar.rb
@@ -12,3 +12,19 @@ p'================================================'
 3.times do
 puts 'bom dia no foco nos estudos'
 end
+p'**************************************************************'
+comidas = ['feijoada' , 'macarronada' , 'peixe frito']
+puts comidas
+p comidas
+
+puts '$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$'
+puts comidas.to_s
+puts '.............................................................'
+puts comidas.join( ' , ')
+puts '***************************************************************'
+puts comidas.join(' :) '  ) + ( ' R$ 8,00 ' )
+puts
+200.times do
+  puts ['gilson']
+end
+
