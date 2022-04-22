b0ca9edb0d540b5873a8ecc3236637aec1da5cbe
gilson dourado <gilsondourado10@gmail.com>
2022-04-22 06:39:40 -0300

    classes

--- treinar.rb
+++ treinar.rb
@@ -1,18 +1,17 @@
 
 
 
-favoritos = []
-# o push adiciona elemento no fim do array
-favoritos.push 'Gilson , Pedro'
-favoritos.push 'Maria, Joana'
-puts favoritos [0]
+a = Array.new +  [1,2,3,4]
+b = String. new + 'ola'
+c = Time.new
+puts 'a = '+a.to_s
+puts 'b = '+b.to_s
+puts 'c = '+c.to_s
+p '***************************************************'
+tempo  = Time.new    #  O instante em que você carrega esta página.
+tempo2 = tempo + 60  #  Um minuto depois.
 
-# o last remove o ultimo elemento e mostra o objeto removido
-puts favoritos.last
-puts favoritos.length
-p'**********************************************'
-# o pop remove o último elemento do array
-puts favoritos.pop
-puts favoritos.length
+puts tempo
+puts tempo2
 
 
