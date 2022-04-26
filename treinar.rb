a18a610ec4899305715a48f3102c35438a166332
gilson dourado <gilsondourado10@gmail.com>
2022-04-26 06:26:33 -0300

    mais sobre classes

--- treinar.rb
+++ treinar.rb
@@ -1,9 +1,10 @@
 
-
+=begin
 class Integer
   def ext
     if self == 5
       porExtenso = 'cinco'
+
     else
       porExtenso = 'cinquenta e oito'
     end
@@ -11,3 +12,43 @@ class Integer
     end
 puts 5.ext
 puts 58.ext
+=end
+p "¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨"
+=begin
+class Dado
+  def rolar
+    1 + rand(5)
+  end
+end
+dados = [ Dado.new , Dado.new, Dado.new ]
+dados.each do |dado|
+  puts dado.rolar
+end
+p'********************************************************************'
+=end
+class Dado
+  def rolar
+    @numeroMostrado = 1+ rand(6)
+    def mostrado
+      @numeroMostrado
+
+    end
+  end
+end
+dado = Dado.new
+dado.rolar
+puts dado.mostrado
+puts dado.mostrado
+dado.rolar
+puts dado.mostrado
+puts dado.mostrado
+p "***************************************************"
+class Dado
+  def rolar
+    @numeroMostrado = 1+ rand(6)
+  end
+  def mostrado
+    @numeroMostrado
+  end
+end
+p Dado.new.mostrado
