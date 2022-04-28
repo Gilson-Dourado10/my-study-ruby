5382d15049a9726bfb610c4c7b924d92636673a1
gilson dourado <gilsondourado10@gmail.com>
2022-04-27 21:40:39 -0300

    initialize

--- treinar.rb
+++ treinar.rb
@@ -1,54 +1,14 @@
 
-=begin
-class Integer
-  def ext
-    if self == 5
-      porExtenso = 'cinco'
 
-    else
-      porExtenso = 'cinquenta e oito'
-    end
-    end
-    end
-puts 5.ext
-puts 58.ext
-=end
-p "¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨"
-=begin
 class Dado
-  def rolar
-    1 + rand(5)
+  def initialize
+    rolar
   end
-end
-dados = [ Dado.new , Dado.new, Dado.new ]
-dados.each do |dado|
-  puts dado.rolar
-end
-p'********************************************************************'
-=end
-class Dado
   def rolar
-    @numeroMostrado = 1+ rand(6)
-    def mostrado
-      @numeroMostrado
-
-    end
-  end
-end
-dado = Dado.new
-dado.rolar
-puts dado.mostrado
-puts dado.mostrado
-dado.rolar
-puts dado.mostrado
-puts dado.mostrado
-p "***************************************************"
-class Dado
-  def rolar
-    @numeroMostrado = 1+ rand(6)
+    @numeroMostrado = 1 + rand(6)
   end
   def mostrado
     @numeroMostrado
   end
 end
-p Dado.new.mostrado
+puts Dado.new.mostrado
