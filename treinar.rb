3a68a47833548c2208cd55702ec2ea6d164ae8ad
gilson dourado <gilsondourado10@gmail.com>
2022-04-29 23:33:21 -0300

    Proc

--- treinar.rb
+++ treinar.rb
@@ -1,115 +1,12 @@
-
-=begin
-class Dado
-  def initialize
-    rolar
+saudacao = Proc.new do
+  puts 'Olá tudo bem!!!'
   end
-  def rolar
-    @numeroMostrado = 1 + rand(6)
-  end
-  def mostrado
-    @numeroMostrado
-  end
+saudacao.call
+saudacao.call
+saudacao.call
+p'*********************************************'
+VoceGostaDe = Proc.new do |umaBoaCoisa|
+  puts 'eu * realmente * gosto de ' + umaBoaCoisa + '!'
 end
-puts Dado.new.mostrado
-=end
-
-class Dragão
-  def initialize nome
-    @nome = nome
-    @dormindo = false
-    @comidaEstomago = 10
-    @comidaIntestino = 0
-    puts @nome + 'nasceu .'
-  end
-  def alimentar
-    puts 'você alimenbtou o ' + @nome + ' . '
-    @comidaEstomago = 10
-    passagemDeTempo
-  end
-  def quintal
-    puts ' você levou o ' + @nome + ' até o quintal. '
-    @comidaIntestino = 0
-    passagemDeTempo
-  end
-  def colocarNaCama
-    puts 'voê colocou o  ' + @nome + ' na cama.'
-    @dormindo = true
-    3.times do
-      if @dormindo
-        passageDeTempo
-      end
-      if @dormindo
-        puts @nome + ' está roncando e enchendo o quarto de fumaça. '
-      end
-    end
-    if @dormindo
-      @dormindo = false
-      puts @nome + ' está acordando. '
-    end
-  end
-  def jogar
-    puts 'voê jopga o ' + @nome + ' no ar. '
-    puts ' ele dá uma risadinha e queima as sobrancelhas. '
-    passagemDeTempo
-  end
-  def balancar
-    puts ' você balança ' + @nome + 'gentilmente. '
-    @dormindo = true
-    puts ' ele começa a cochilar... '
-    passagemDeTempo
-    if @dormindo
-      @dormindo = false
-      puts '... mas acorda quando vocẽ pára.  '
-    end
-  end
-  private
-  def comFome?
-    @comidaEstomago <= 2
-  end
-  def precisaSair?
-    @comidaIntestino >= 8
-  end
-  def passagemDeTempo
-    if @comidaEstomago > 0
-      @comidaEstomago = @comidaEstomago -1
-      @comidaIntestino = @comidaIntestino +1
-    else
-      if @dormindo
-        @dormindo = false
-        puts 'ele está acordado!'
-      end
-      puts @nome + ' ele está faminto! em desepero ele comeu VOCÊ!'
-      exit
-
-    end
-    if @comidaIntestino >= 10
-      @comidaIntestino = 0
-      puts ' Ops' + @nome + 'teve um acidente...'
-    end
-    if comFome?
-      if @dormindo
-        @dormindo = false
-        puts ' ele está acordado '
-      end
-      puts ' o estômago do ' + @nome + ' está roncando... '
-    end
-    if precisaSair?
-      if @dormindo
-        @dormindo = false
-        puts ' ele estpá acordado!'
-      end
-      puts @nome + ' faz a dança para ir ao quintal... '
-    end
-  end
-end
-bichinho = Dragão.new 'Norbert'
-bichinho.alimentar
-bichinho.jogar
-bichinho.quintal
-bichinho.colocarNaCama
-bichinho.balancar
-bichinho.colocarNaCama
-bichinho.colocarNaCama
-bichinho.colocarNaCama
-bichinho.colocarNaCama
+VoceGostaDe.call 'Bolo de milho'
+VoceGostaDe.call 'Estudar Ruby'
