f4903bb90219e8d6fcde98e5323dfcf4d1cbadd6
gilson dourado <gilsondourado10@gmail.com>
2022-05-15 07:32:59 -0300

    classes

--- class.rb
+++ class.rb
@@ -1,14 +1,13 @@
-def h
-  puts " bom dia Gilson"
-end
-puts h
+class MinhaClasse
+  end
+    objeto = MinhaClasse.new
 
-def h nome
-  puts "ola #{nome}!"
-end
-h ("Gilson")
-
-def h (nome = " Mundo")
-  puts "olá #{nome.capitalize}!"
-end
-h "Pedro"
+puts  objeto.object_id
+p'*****************************************************************'
+foo = "UMA STRING"
+bar = foo.downcase
+foo.downcase! # o ponto de exclamação modifica o objeto original
+p foo
+p bar
+p foo
+p bar
