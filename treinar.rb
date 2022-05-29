28de1863a4479d714a225034c34bb55f159b3e42
gilson dourado <gilsondourado10@gmail.com>
2022-05-29 11:47:43 -0300

    listas com interpolação

--- treinar.rb
+++ treinar.rb
@@ -1,26 +1,14 @@
+# listas com interpolação
 
-class Carro
-  attr_reader :marca, :modelo, :tanque
-  attr_accessor :cor
+outro_nomes = ["maria ", "josé"]
+nomes = ["gilson", "pedro",10, true,nil ] + [ "pedro", "joão"] + ["#{outro_nomes}" ]
+puts nomes
+p'****************************************************************************************'
+lista_atrasados = ["Tia Cláudia","Tio Antônio"]
+lista_presenca_convidados = ["Davi"," Maria", "douglas"] + [" Prima Cátia"]
+lista_penetras = [" Serginho boca preta", "Anão da 13", " Berruga"] + ["#{lista_atrasados}"]
 
-  def initialize(marca, modelo , cor, tanque)
-    @marca   = marca
-    @modelo  = modelo
-    @cor    = cor
-    @tanque = tanque
+puts lista_presenca_convidados + lista_penetras
 
-  end
-  def to_s
-    "Marca: #{@marca} Modelo: #{@modelo}  Cor #{@cor} tanque #{@tanque} "
-  end
-end
 
-corsa = Carro.new(:chevrolet , :corsa, :preto, 50)
-gol   = Carro.new(:volks, :gol, :azul, 42)
-palio = Carro.new(:fiat, :palio, :vermelho, 45)
-Carro.send( :define_method, "multiplica_tanque") do |valor|
-  @tanque * valor
-end
-puts corsa.multiplica_tanque(2)
-puts gol.multiplica_tanque(2)
-puts palio.multiplica_tanque(1.5)
+
