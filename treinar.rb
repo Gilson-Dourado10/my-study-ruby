37c217b1dd0efe33ace861df97854e68836cbc35
gilson dourado <gilsondourado10@gmail.com>
2022-05-31 18:33:47 -0300

    atributos virtuais

--- treinar.rb
+++ treinar.rb
@@ -1,14 +1,22 @@
-# listas com interpolação
-
-outro_nomes = ["maria ", "josé"]
-nomes = ["gilson", "pedro",10, true,nil ] + [ "pedro", "joão"] + ["#{outro_nomes}" ]
-puts nomes
-p'****************************************************************************************'
-lista_atrasados = ["Tia Cláudia","Tio Antônio"]
-lista_presenca_convidados = ["Davi"," Maria", "douglas"] + [" Prima Cátia"]
-lista_penetras = [" Serginho boca preta", "Anão da 13", " Berruga"] + ["#{lista_atrasados}"]
-
-puts lista_presenca_convidados + lista_penetras
 
 
+class Carro
 
+  attr_reader :marca, :modelo, :tanque
+  attr_accessor :cor
+  def initialize(marca, modelo, cor, tanque)
+    @marca = marca
+    @modelo = modelo
+    @cor    = cor
+    @tanque = tanque
+  end
+  def to_s
+    "Marca:#{@marca} Modelo:#{@modelo} Cor:#{@cor} Tanque:#{@tanque}"
+  end
+  def galoes
+    @tanque / 5.0
+  end
+end
+corsa = Carro.new(:chevrolet, :corsa, :preto, 50)
+corsa.cor = :branco
+puts " o cliente precisaria de #{corsa.galoes}  galões de gasolina  para encher o tanque de #{corsa.tanque}  litros pra o seu carro corsa!!"
