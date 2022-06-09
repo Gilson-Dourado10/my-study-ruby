807a8d87592b74b4029b1e8d288366fc1f9044ce
gilson dourado <gilsondourado10@gmail.com>
2022-06-08 23:49:16 -0300

    array, hash

---
+++ treinar.rb
@@ -0,0 +1,10 @@
+array_c = %w(curso ruby com jakcson pires)
+puts array_c
+p'*********************************************************'
+meu_hash = {abc:"rails"}
+p meu_hash
+p"******************************************************"
+array_c.size.eql?(3)
+array_c.size.eql?(2)
+puts array_c
+puts array_c
