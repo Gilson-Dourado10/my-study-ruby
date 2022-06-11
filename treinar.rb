8fa9c7b8b81c531bf623e66503aeb6abb181376a
gilson dourado <gilsondourado10@gmail.com>
2022-06-10 23:42:45 -0300

    parametros

--- treinar.rb
+++ treinar.rb
@@ -1,10 +1,6 @@
-array_c = %w(curso ruby com jakcson pires)
-puts array_c
-p'*********************************************************'
-meu_hash = {abc:"rails"}
-p meu_hash
-p"******************************************************"
-array_c.size.eql?(3)
-array_c.size.eql?(2)
-puts array_c
-puts array_c
+def mtd (arg1 = "telhado" , arg2 = "cadeira", arg3 = "música")
+  "#{arg1}, #{arg2}, #{arg3}"
+  end
+  puts mtd
+  p mtd ("ruby, java")
+
