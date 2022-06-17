c8b4c9c54e551217135e84e0541f045458bb16f8
gilson dourado <gilsondourado10@gmail.com>
2022-06-16 23:33:32 -0300

    herança

--- treinar.rb
+++ treinar.rb
@@ -1,32 +1,16 @@
 class Teste
+  def metodo(parametro1)
+    puts parametro1
+  end
+end
 
+class Novo_Teste < Teste
+  def metodo(parametro1 , parametro2)
+    super(parametro1)
+    puts parametro2
+  end
 end
-class OutroTeste < Teste
-
-end
-p String.superclass
-p Teste.superclass
-p OutroTeste.superclass
-
-p"*****************************************************"
-class Carro
-
-end
-attr_reader :marca , :modelo, :tanque
-attr_acessor :cor
-def initialize(marca , modelo , cor ,tanque)
-  @marca
-  @modelo
-  @cor
-  @tanque
-end
-def to_s
-  " Marca: #{@marca} Modelo: #{@modelo}  Cor: #{@cor} Tanque: #{@tanque}  "
-end
-corsa = Carro.new(:chevrolet ,:corsa ,:preto, :50  )
-gol = Carro.new(:volks , :gol ,:azul,:42)
-Carro.send(:define_method, "multiplica_tanque") do |valor|
-  @tanque * valor
-end
-puts corsa.multiplica_tanque(2)
-puts gol.multiplica_tanque(2)
+t1 = Teste.new
+t2 = Novo_Teste.new
+t1.metodo(1)
+t2.metodo(2,3)
