cb7d945395e9ef795f1993014499d564e4efa574
gilson dourado <gilsondourado10@gmail.com>
2022-06-17 23:13:58 -0300

    struct in ruby

--- treinar.rb
+++ treinar.rb
@@ -1,16 +1,8 @@
-class Teste
-  def metodo(parametro1)
-    puts parametro1
-  end
-end
 
-class Novo_Teste < Teste
-  def metodo(parametro1 , parametro2)
-    super(parametro1)
-    puts parametro2
+class Carro < Struct.new( :marca  , :modelo , :cor , :tanque)
+  def to_s
+    "Marca:  #{marca}  Modelo: #{modelo} Cor: #{cor} Tanque: #{tanque} "
   end
 end
-t1 = Teste.new
-t2 = Novo_Teste.new
-t1.metodo(1)
-t2.metodo(2,3)
+fox = Carro.new(:vw , :fox ,:verde , 45)
+puts fox
