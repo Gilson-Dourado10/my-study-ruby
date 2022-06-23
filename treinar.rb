b98ad1ee2ef8ea1653750f8ffe887e1270200744
gilson dourado <gilsondourado10@gmail.com>
2022-06-22 23:53:05 -0300

    mixins , módulos

--- treinar.rb
+++ treinar.rb
@@ -1,29 +1,26 @@
-class CaixaDeParafusos
-  protected
-  attr_writer :quantidade
-  public
-  attr_reader :quantidade
-  def initialize(quantidade)
-    @quantidade = quantidade
+class Primata
+  def come
+    puts "Nham!"
   end
-  def to_s
-    "Quantidade de parafusos na caixa #{self.object_id}: #{@quantidade}"
-  end
-  def +(outra)
-    nova = CaixaDeParafusos.new(@quantidade + outra.quantidade)
-    @quantidade = 0
-    outra.quantidade = 0
-    nova
-  end
-  def /(quantidade)
-    caixas = Array.new(quantidade, @quantidade / quantidade)
-    (@quantidade % quantidade).times { |indice| caixas[indice] += 1 }
-    @quantidade = 0
-    caixas.map { |quantidade| CaixaDeParafusos.new(quantidade) }
+  def dorme
+    puts "Zzzzzz..."
   end
 end
-caixa1 = CaixaDeParafusos.new(10)
-caixa2 = CaixaDeParafusos.new(20)
-caixa3 = caixa1 + caixa2
-
-puts caixa3 / 4
+class Humano < Primata
+  def conecta_na_web
+    puts "Login ... senha ..."
+  end
+end
+module Ave
+  def voa
+    puts "Para o alto, e avante!"
+  end
+end
+class Mutante < Humano
+  include Ave
+end
+mutante = Mutante.new
+mutante.come
+mutante.dorme
+mutante.conecta_na_web
+mutante.voa
