2ce4fbaf819af0c8152b4da3362119028af47004
gilson dourado <gilsondourado10@gmail.com>
2022-07-24 23:54:40 -0300

    método initialize

--- treinar.rb
+++ treinar.rb
@@ -1,28 +1,18 @@
-class Exemplo
-  @@variavel_de_classe = "Variável da classe"
 
-  def initialize(variavel_de_instancia)
-    @variavel_de_instancia = variavel_de_instancia
+  class Person
+    def initialize(name)
+      @name = name
+    end
+  end
+  def name
+    @name
   end
 
-  def variavel_de_instancia
-    @variavel_de_instancia
+  def password=(password)
+    @password = password
   end
+  person = Person.new("Ada")
+  person.password=("super secret")
+  p person
 
-  def self.variavel_de_instancia
-    @variavel_de_instancia
-  end
 
-  def variavel_de_classe
-    @@variavel_de_classe
-  end
-
-  def self.variavel_de_classe
-    @@variavel_de_classe
-  end
-end
-x = Exemplo.new("Instância 1")
-puts x.variavel_de_classe
-puts x.variavel_de_instancia
-puts Exemplo.variavel_de_classe
-p Exemplo.variavel_de_instancia
