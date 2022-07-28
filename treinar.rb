6f40b6d39af450aeb2186006bd3ec05f626eafdd
gilson dourado <gilsondourado10@gmail.com>
2022-07-28 00:03:55 -0300

    metodo hash

--- treinar.rb
+++ treinar.rb
@@ -1,8 +1,11 @@
-
-name = "Jane"
-
-puts "Name is: #{name}"
-
-puts "Self is: #{self}"
-
-puts "Self class is: #{self.class}"
+# hash
+h = { foo:  0 , :bar  =>  1 , 'baz':  2 }
+puts h
+p'******************************************************'
+pessoa = { nome:  'Matz' , idioma:  'Ruby' }
+p pessoa
+p'************************************************************'
+def  some_method ( hash )
+  p  hash
+end
+some_method ({ foo:  10 , bar:  51 , baz:  2 })
