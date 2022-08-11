ff925c43bb8884f1479f6bbb7433084d5284e14d
gilson dourado <gilsondourado10@gmail.com>
2022-08-10 23:43:05 -0300

    hash

--- treinar.rb
+++ treinar.rb
@@ -1,30 +1,9 @@
-# struct
-    Notebook= Struct.new( :cpu , :ssd, :ram   )
-    notebook = Notebook.new( "i7" ,"16gb", " 256ssd" )
-p notebook.cpu
-p   notebook.ram
-p   notebook.ssd
-p'************************************************************'
-Note = Struct.new(:cpu , :ram, :ssd ) do
-  def  all_configs
-      "Pc configs: #{cpu} - #{ram}  - #{ssd}"
+class Dev
+  attr_accessor :name, :language
+  def initialize(hash)
+    self.name = hash[:name]
+    self.language = hash[:language]
   end
 end
-puts notebook = Note.new("i9", "32gb ", "1tb" )
-
-
-p'*******************************************************************'
-
-Status = Struct.new(success?, errors)
-
-def initialize(request_params)
-  @request_params = request_params
-end
-
-def call
-  if @request_params[:status].present? && @request_params[:status] == "success"
-    status = Status.new(true, nil)
-  else
-    status = Status.new(false, @request_params[:errors])
-  end
-end
+matz = Dev.new(name: 'Matz', language: 'Ruby')
+puts Dev
