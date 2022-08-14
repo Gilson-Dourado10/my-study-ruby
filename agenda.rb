cb5a64761507331656e226a9d62872aa637c1acb
gilson dourado <gilsondourado10@gmail.com>
2022-08-13 23:56:42 -0300

    feat:(implemented)agenda ruby puro

--- agenda.rb
+++ agenda.rb
@@ -4,13 +4,21 @@
   @genda =[ {nome: " Daniel" ,telefone: "91234-5656" } ,
             {nome: " Samuel" ,telefone: "94321-5656" }
            ]
-  loop do
+  def todos_contatos
+    @agenda.each do |contato|
+      puts "#{contato[:nome]}  - #{contato[:telefone]}"
+    end
+  end
 
+  loop do
   puts "1. Contatos\n2. Adicionar contato\n3. Ver Contato\n4. Editar Contato\n5. Remover Contato\n0. Sair  "
   codigo = gets.chomp.to_i
   case
   when codigo == 0
   puts " até logo!!! "
   break
-end
+  when codigo == 1
+  todos_contatos
+  break
+  end
 end
