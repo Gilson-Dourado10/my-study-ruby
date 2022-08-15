8d3cf2c59f7125dab6d84dec6569c8c330c7f728
gilson dourado <gilsondourado10@gmail.com>
2022-08-14 23:56:28 -0300

    feat:(implemented)agenda ruby puro

--- agenda.rb
+++ agenda.rb
@@ -5,20 +5,32 @@
             {nome: " Samuel" ,telefone: "94321-5656" }
            ]
   def todos_contatos
-    @agenda.each do |contato|
-      puts "#{contato[:nome]}  - #{contato[:telefone]}"
+    @agenda.each do  |contato|
+      puts "#{contato[:nome]} - #{contato[:telefone]}"
     end
+    puts "*********************************************************"
+  end
+def adicionar_contato
+  print " Nome:"
+  nome = gets.chomp
+  print "Telefone:"
+  telefone = gets.chomp
+  @agenda<< {nome: nome , telefone: telefone }
   end
 
   loop do
   puts "1. Contatos\n2. Adicionar contato\n3. Ver Contato\n4. Editar Contato\n5. Remover Contato\n0. Sair  "
   codigo = gets.chomp.to_i
+
   case
   when codigo == 0
-  puts " até logo!!! "
+    puts " até logo!!! "
   break
+
   when codigo == 1
-  todos_contatos
+      todos_contatos
   break
+  when codigo == 2
+    adicionar_contato
   end
 end
