dd413c819203665923dd4b7a1583a82041d29376
gilson dourado <gilsondourado10@gmail.com>
2022-08-19 23:56:42 -0300

    feat:(implemented symbol)

--- teste.rb
+++ teste.rb
@@ -1,4 +1,17 @@
-def  helloword
-    puts "olá gilson, tudo bem? "
-end
-helloword
+module One
+
+    class Fred
+    end
+    $f1 = :Fred
+  end
+  module Two
+    Fred = 1
+    $f2 = :Fred
+  end
+  def Fred()
+  end
+  
+  $f3 = :Fred
+ puts  $f1.object_id  
+ puts  $f2.object_id  
+puts  $f3.object_id 
