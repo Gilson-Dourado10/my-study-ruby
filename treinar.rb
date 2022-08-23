f4936a9ba5221ad7ef6f7ca9b75b33d957e32e7c
gilson dourado <gilsondourado10@gmail.com>
2022-08-22 23:51:23 -0300

    feat:(métodos com parametros )

--- treinar.rb
+++ treinar.rb
@@ -20,3 +20,11 @@
  end
 
  puts(ola2 'talim')
+
+p"**********************************************************************"
+
+def mtd(arg1="Dibya", arg2="Shashank", arg3="Shashank")
+   "#{arg1}, #{arg2}, #{arg3}."
+   end
+puts mtd
+ puts mtd("ruby")
