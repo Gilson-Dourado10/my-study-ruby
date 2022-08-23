2897a37146539e4666ffdf7f1213dfd3a8577636
gilson dourado <gilsondourado10@gmail.com>
2022-08-22 23:48:34 -0300

    feat:(métodos com parametros )

--- treinar.rb
+++ treinar.rb
@@ -1,8 +1,22 @@
-pal = "Never a foot too far, even."
-class String
-  def palindrome?
-    letters = self.downcase.scan(/\w/)
-    letters == letters.reverse
-  end
-end
-puts pal.palindrome?
+# Um método simples
+ def ola
+   puts "Olá"
+ end
+ #uso do método
+ ola
+
+ # Método com argumentos - 1
+ def ola1(nome)
+   puts 'Olá ' + nome
+   return 'sucesso'
+ end
+
+ puts(ola1('satish'))
+
+ # Método com argumentos - 2
+ def ola2 nome2
+   puts 'Olá ' + nome2
+   return 'sucesso'
+ end
+
+ puts(ola2 'talim')
