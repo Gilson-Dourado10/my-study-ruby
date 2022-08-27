b545fc9ac0fef05f82c17edf1b3f03d2fd958a17
gilson dourado <gilsondourado10@gmail.com>
2022-08-26 23:58:13 -0300

    feat:(operadores matemáticos) saida  de dados

--- symbol.rb
+++ symbol.rb
@@ -1,14 +1,48 @@
-# SAÍDA DE DADOS
-
-print "digite o seu nome:"
-nome = gets.chomp
-print " digite  a sua idade: "
-idade = gets.chomp
-puts "hello #{nome} e minha idade é #{idade}"
-
-p'************************************************************'
-print " digite o nome da sua mãe: "
-mae = gets.chomp
-print" digite o nome do seu pai: "
-pai = gets.chomp
-puts " o nome da sua mãe #{mae} e o nome do seu pai #{pai} cuidam muito bem de você!!!"
+# operadores  matemáticos
+print " digite um número inteiro: "
+num1 = gets.chomp.to_i
+print " digite segundo m número inteiro: "
+num2 = gets.chomp.to_i
+soma = num1 + num2
+puts " a soma dos números #{num1} + #{num2} é:  #{soma}"
+p'****************************************************************** '
+# operadores  matemáticos
+print " digite um número inteiro: "
+num1 = gets.chomp.to_i
+print " digite segundo  número inteiro: "
+num2 = gets.chomp.to_i
+divisao  = num1 / num2
+puts " a divisão dos números #{num1} / #{num2} é:  #{divisao}"
+p'****************************************************************** '
+# operadores  matemáticos
+print " digite um número inteiro: "
+num1 = gets.chomp.to_i
+print " digite segundo m número inteiro: "
+num2 = gets.chomp.to_i
+multiplicacao  = num1 * num2
+puts " a multiplicao dos números #{num1} * #{num2} é:  #{multiplicacao}"
+p'****************************************************************** '
+# operadores  matemáticos
+print " digite um número inteiro: "
+num1 = gets.chomp.to_i
+print " digite segundo m número inteiro: "
+num2 = gets.chomp.to_i
+subtracao  = num1 - num2
+puts " a subtracao  dos números #{num1} - #{num2} é:  #{subtracao}"
+p'****************************************************************** '
+# operadores  matemáticos
+print " digite um número inteiro: "
+num1 = gets.chomp.to_i
+print " digite segundo m número inteiro: "
+num2 = gets.chomp.to_i
+modulo = num1 % num2
+puts " a modulo dos números #{num1} %  #{num2} é:  #{modulo}"
+p'****************************************************************** '
+# operadores  matemáticos
+print " digite um número inteiro: "
+num1 = gets.chomp.to_i
+print " digite segundo m número inteiro: "
+num2 = gets.chomp.to_i
+potenciacao = num1 ** num2
+puts " a potenciação  dos números #{num1} ** #{num2} é:  #{potenciacao}"
+p'****************************************************************** '
