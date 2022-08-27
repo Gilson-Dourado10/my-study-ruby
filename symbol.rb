84b43cc7d92c9a01de07f5a0972d51056ba46e7d
gilson dourado <gilsondourado10@gmail.com>
2022-08-26 23:40:02 -0300

    feat:(hash) saida  de dados

--- symbol.rb
+++ symbol.rb
@@ -1,7 +1,14 @@
-# Tipos de dados hash
+# SAÍDA DE DADOS
 
-hash = { curso: 'ruby puro' , linguagem: 'ruby' , plataforma: "youtube"  }
-p hash
-p hash[:curso]
-p hash[:linguagem]
-p hash[:plataforma]
+print "digite o seu nome:"
+nome = gets.chomp
+print " digite  a sua idade: "
+idade = gets.chomp
+puts "hello #{nome} e minha idade é #{idade}"
+
+p'************************************************************'
+print " digite o nome da sua mãe: "
+mae = gets.chomp
+print" digite o nome do seu pai: "
+pai = gets.chomp
+puts " o nome da sua mãe #{mae} e o nome do seu pai #{pai} cuidam muito bem de você!!!"
