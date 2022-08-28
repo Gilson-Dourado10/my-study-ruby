1731512a194d7ea60c8fb2e81e394d858d2d0472
gilson dourado <gilsondourado10@gmail.com>
2022-08-28 07:39:32 -0300

    feat:(condicional )estrututa de controle

--- if.rb
+++ if.rb
@@ -1,7 +1,21 @@
 # "IF " estrutura de controle
 
-dia =  " domingo"
-if  dia != " domingo"
-  comida = "churrasco"
+day = 'sunday'
+if day == 'sunday'
+  lunch = 'special '
 end
-puts " hoje vai ser  #{ comida }  almoço "
+puts " lunch is #{lunch} today"
+
+puts " ***********************************************************************************"
+num =  3 + 1 * 2
+if num <= 4
+  numero = " integer"
+end
+puts " o número é #{numero}   no ruby"
+
+puts '¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨'
+dia = "7"
+unless dia == "7"
+  dia_da_semana = "domingo"
+  end
+puts " hoje é #{dia_da_semana} da semana"
--- unless.rb
+++ unless.rb
@@ -1,8 +1,22 @@
 
 # "condicional unless" EStrutura de controle
 a = 1
-unless a >= 1
-  puts " Bem Vindo "
-else
+unless a != 1
   puts " Olá"
+else
+  puts " Bem Vindo"
+end
+puts"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+x = 2
+unless x == 2
+  puts "x é diferente de 2"
+else
+  puts "x é igual a 2"
+end
+p"$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
+x = 1
+unless x>=2
+  puts "x é menor que  2"
+else
+  puts "x é maior que  2"
 end
