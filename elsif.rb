79df1d6c4eeea5a2a7dcafa51a528d870d0f58e3
gilson dourado <gilsondourado10@gmail.com>
2022-08-27 23:55:55 -0300

    feat:(condicional  elsif )estrututa de comtrole

---
+++ elsif.rb
@@ -0,0 +1,10 @@
+# "condicional elsif" EStrutura de controle
+
+dia = "quarta "
+if dia == " quinta"
+  puts " hoje é #{dia}"
+elsif  dia == " segunda "
+  puts " hoje é #{dia}"
+else
+  puts " é outro dia da semana "
+end
