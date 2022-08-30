55e4e9b95bd5200c1805cf28d06922de01aa860a
gilson dourado <gilsondourado10@gmail.com>
2022-08-29 23:52:25 -0300

    feat:(times  )estrututa de controle

--- times.rb
+++ times.rb
@@ -2,3 +2,8 @@
   3.times  do
     puts " aprendendo ruby puro"
   end
+  p"----------------------------------------------------------------------"
+  materia = ["MATEMÁTICA", "FÍSICA" ,"QUÍMICA ", " PORTUGUÊS " ]
+  4.times do |index|
+      puts materia [index]
+  end
