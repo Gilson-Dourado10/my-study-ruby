346042e4b38882a565ed84a248fd208d21d33568
gilson dourado <gilsondourado10@gmail.com>
2022-09-02 23:59:04 -0300

    feat:(iteração e condicional/escola  )estrututa de controle

---
+++ escola.rb
@@ -0,0 +1,16 @@
+escola = ''
+loop do
+  puts escola
+  puts 'selecione uma das seguintes opções'
+  puts '1- Aluno '
+  puts '2- Professor'
+  puts '0- Sair'
+  print ' Opção: '
+  opcao = gets.chomp.to_i
+end
+if opcao == 1
+  print 'digite o nome do aluno: '
+  nome_aluno == gets.chomp.to_s
+
+
+end
