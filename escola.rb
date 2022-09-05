a02c9d0df8ac160c9211d28a6a6f5a4866aa5fff
gilson dourado <gilsondourado10@gmail.com>
2022-09-04 23:20:57 -0300

    feat:(iteração e condicional )escola

--- escola.rb
+++ escola.rb
@@ -7,10 +7,14 @@ loop do
   puts '0- Sair'
   print ' Opção: '
   opcao = gets.chomp.to_i
-end
+
 if opcao == 1
   print 'digite o nome do aluno: '
-  nome_aluno == gets.chomp.to_s
-
-
+  nome_aluno = gets.chomp.to_s
+  print ' digite qual a sala que você estuda: '
+  sala_aluno = gets.chomp.to_i
+  print 'qual o curso você esta cursando: '
+  curso_aluno = gets.chomp.to_s
+  escola = "o aluno #{nome_aluno} estuda na sala #{sala_aluno} e está cursando o #{curso_aluno} de graduação"
+  end
 end
