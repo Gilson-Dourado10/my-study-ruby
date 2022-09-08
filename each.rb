e635ab19176702117beb5e3b208b1334e3165d8b
gilson dourado <gilsondourado10@gmail.com>
2022-09-07 23:34:10 -0300

    feat:(array)iteração

---
+++ each.rb
@@ -0,0 +1,14 @@
+# diferença MAP, SELECT e EACH
+
+#ARRAY COMUM
+=begin
+places = ['restaurant', 'mall', 'park', 'theater']
+ for place in places
+   puts place
+ end
+=end
+frutas = ["melancia", "melão","uva" , "tangerina"]
+for  fruta in frutas
+  puts frutas
+
+end
