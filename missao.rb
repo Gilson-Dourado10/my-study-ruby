17a1e9c87b2f12741b74cc1b020c68e27416587a
gilson dourado <gilsondourado10@gmail.com>
2022-09-09 23:41:39 -0300

    feat:(map! )collection

--- missao.rb
+++ missao.rb
@@ -1,13 +1,20 @@
 
-missao = [2,3,4]
+missao = [2,4,8]
 missao.map! do |num|
   num **2
 end
 puts "#{missao}"
 
 p"**************************"
-array =[1,2,3]
+array =[2,2,2]
 array.map! do |a|
-  a * 2
+  a ** 2
 end
 puts " #{array}"
+
+p"################################"
+array = [1,5,10]
+new_array = array.map! do |a|
+  a * 2
+end
+p array
