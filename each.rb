2af60ffb66344f179e82b28671fd519a17a88624
gilson dourado <gilsondourado10@gmail.com>
2022-09-09 23:03:04 -0300

    feat:(hash com each )

--- each.rb
+++ each.rb
@@ -1,53 +1,9 @@
-# diferença MAP, SELECT e EACH
-
-#ARRAY COMUM
-=begin
-places = ['restaurant', 'mall', 'park', 'theater']
- for place in places
-   puts place
- end
-
-frutas = ["melancia", "melão","uva" , "tangerina"]
-for  fruta in frutas
-  puts frutas
 
 
 
-linguagens = [" RUBY","C++", "JAVA","DELPHI ", "PHP", "GOLANG"]
-linguagens.each do |linguagem|
-    puts linguagem
-    end
-p'*****************************************************************************'
-
-nomes = ["pedro ", " sandra", "joaquim", " leoncio","joana"]
-nomes.each { |nome | puts nome }
 
 
 
-m2 = [ 1,2,3,4,5]
-m2.each do |m2|
-  puts m2
-end
-
-m2 = [1,2,2,3,3,3]
-m2.each { |x| p x}
-
-nome = ["maria", 1, false,true ," bola", 3*3 ]
-nome.each { |var| puts var }
-
-futebol = [" palmeiras", "são paulo", "corinthians", "botafogo", "flamengo" ]
-futebol.each {|p| puts futebol}
-
-n1 = 1
-n2 = 3
-n3 = 10
-n4 = 4
-# verifica se o número é impar com o true ou false
-p n1.odd?
-p n2.odd?
-p n3.odd?
-p n4.odd?
-=end
 aulas = {'Aula 1 ' => 'liberada', 'Aula 2 ' => 'liberada', 'Aula 3 ' => 'liberada', 'Aula 4 ' => 'liberada', 'Aula 5 ' => 'em breve'}
 
 aulas.each do |key, value|
@@ -55,6 +11,6 @@ aulas.each do |key, value|
 end
 
 aulas = {'Aula 1 ' => 'liberada', 'Aula 2 ' => 'liberada', 'Aula 3 ' => 'liberada', 'Aula 4 ' => 'liberada', 'Aula 5 ' => 'em breve'}
-aulas = {Aula1: 'liberada', Aula2: 'liberada'}
+curso = {Aula1: 'liberada', Aula2: 'liberada'}
 
-#capitais = { acre: 'Rio Branco', sao_paulo: 'São Paulo'}
+p capitais = { acre: 'Rio Branco', sao_paulo: 'São Paulo'}
