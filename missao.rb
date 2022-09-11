0222ff4e8bee2a3a64317a4bac66f5f464b9238d
gilson dourado <gilsondourado10@gmail.com>
2022-09-11 00:17:46 -0300

    feat:(hash )collection

--- missao.rb
+++ missao.rb
@@ -2,3 +2,8 @@
 Numbers = {a: 10, b: 30, c: 20, d: 25, e: 15}
 
 puts "A chave de maior valor é #{Numbers.key(Numbers.values.max)} e o elemento de maior valor é #{Numbers.values.max}"
+
+
+letras = {a: "AAA" , a: 'AAAA' , a: "AAAAA"  }
+puts "a chave de maior valor é #{letras.key(letras.values.max)} e o elemento de maior valor é:  #{letras.values.max} "
+
