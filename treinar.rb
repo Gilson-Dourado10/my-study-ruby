1ca0cd5dbdae23dfa56961ae5cb7e5e925c6d890
gilson dourado <gilsondourado10@gmail.com>
2022-09-13 00:05:53 -0300

    feat:(métodos ) com parámetros

--- treinar.rb
+++ treinar.rb
@@ -8,4 +8,12 @@ last_name = 'DOURADO'
 
 talk(first_name, last_name)
 
+puts "*************************************************"
+def parametro(parametro1 , parametro2)
+  puts "o primeiro parámetro é:#{parametro1}  e o segundo parámetro #{parametro2}"
+  end
+  parametro1= "programação"
+  parametro2 = " ruby on rails"
+    parametro(parametro1,parametro2)
+
 
