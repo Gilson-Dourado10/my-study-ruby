a605a4fa7fc7d3badc08608e4f17d92677b07989
gilson dourado <gilsondourado10@gmail.com>
2022-09-14 00:39:21 -0300

    feat:(classes) attr_reader

--- classes.rb
+++ classes.rb
@@ -1,3 +1,4 @@
+=begin
 class Pessoa
   attr_accessor :nome
   attr_accessor :idade
@@ -18,9 +19,33 @@ end
 pessoa1 = Pessoa.new("Pedro Antônio", 37)
 pessoa2 = Pessoa.new(" geraldo Luiz", 50)
 puts pessoa1.nome
+pessoa2.nome =" maria"
 puts pessoa1.idade
 p"--------------------------"
 puts pessoa2.nome
 puts pessoa2.idade
+=end
+class Cachorro
+  attr_reader :nome
+  attr_reader :raca
 
+  def initialize(nome , raca)
+    @nome = nome
+    @raca = raca
+  end
 
+  def latir(texto ="auauuuuu")
+    "latindo #{texto}"
+  end
+end
+
+cachorro1 = Cachorro.new("max","pitcher")
+cachorro2 = Cachorro.new("besteira","viralata")
+
+puts cachorro1.raca
+puts cachorro1.nome
+
+puts "**************************************"
+
+puts cachorro2.raca
+puts cachorro2.nome
