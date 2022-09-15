19edce5e7f85ef29fa86445f9487ec43720da840
gilson dourado <gilsondourado10@gmail.com>
2022-09-14 23:50:44 -0300

    feat:(self) dentro de outra classe

--- self.rb
+++ self.rb
@@ -3,5 +3,15 @@ class TesteSelf
     puts "esse é meu self: #{self }"
   end
 end
+p"*********************************"
+
+class Teste1
+  def meu_self
+    puts "esse é o meu self: #{self }"
+  end
+end
+t = Teste1.new
+t.meu_self
+
 teste = TesteSelf.new
 teste.ola
