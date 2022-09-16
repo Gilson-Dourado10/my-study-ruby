d4836c98b12204b7e88c3be0642c090488c578e7
gilson dourado <gilsondourado10@gmail.com>
2022-09-16 00:00:26 -0300

    feat:(classes)variáveis de instância

--- classes.rb
+++ classes.rb
@@ -1,13 +1,29 @@
-class Carro
-  # puts"###"
-  def andar
-    ######
+class Lampada
+  def ligar
   end
 
+  def desligar
+  end
 end
 
+class Conta
+  def depositar
+  end
+
+  def sacar
+  end
+
+  def consultar_saldo(saldo = 100)
+    @saldo =saldo
+  end
+
+  def transferir
+  end
+end
+
+conta = Conta.new
+puts conta.consultar_saldo
 
 
 
-carro = Carro.new
-carro
+
