c0705658afdff3940d8c487c1abb1707d2d3e841
gilson dourado <gilsondourado10@gmail.com>
2022-09-17 09:54:47 -0300

    feat:(symbol) de vaŕias formas

--- symbol.rb
+++ symbol.rb
@@ -9,8 +9,8 @@ class S
    end
 end
 
-  saber_ruby = :sim
-    if saber_ruby == :sim
+  saber_ruby = "sim"
+    if saber_ruby == "sim"
        puts  "vocé desenvolvedor ruby jr"
     else
        puts  " vocẽ precisa aprender Ruby urgente "
