b2e1a26f06ff6417cb4ca79c263a7b97bad62f8c
gilson dourado <gilsondourado10@gmail.com>
2022-09-18 17:34:39 -0300

    feat:(classes) de vaŕias formas

--- classes.rb
+++ classes.rb
@@ -1,23 +1,28 @@
-class Thing
-  def square(n)
-    n*n
+
+class Cachorro
+  def initialize(nome , raça )
+    @nome = nome
+    @raça = raça
+  end
+  def late
+    puts "AU AU"
+  end
+  def mostra
+    puts " o nome do cachorro É: #{@nome} e a raçã é: #{@raça}"
+  end
+  def anda
+    puts "andando devagar "
   end
 end
-thing = Thing.new
-meth  = thing.method(:square)
-
-p meth.call(9)                 #=> 81
-p [ 1, 2, 3,4 ].collect(&meth)   #=> [1, 4, 9]
-
-[ 1, 2, 3 ].each(&method(:puts)) #=> prints 1, 2, 3
-
-class Pessoa
-  def calc(x)
-    x+x
-  end
+cachorro = Cachorro.new("Viralata", "Felix")
+if cachorro.respond_to? ("anda"  )
+  cachorro.late
+else
+  puts " é possível o cachorro está doente 'anda' "
 end
-  pessoa = Pessoa.new
-  nomes = pessoa.method(:calc)
-  p nomes.call(3)
-  p [1,2].each.collect(&pessoa)
-[1,2].each(&method(:puts))
+puts " o id do objeto é :  #{cachorro.object_id}"
+cachorro.late
+cachorro.mostra
+cachorro.anda
+
+
