4511958fadc7268593d947bca7a7a3b3719ce777
gilson dourado <gilsondourado10@gmail.com>
2022-09-19 23:39:25 -0300

    feat:(attr_reader e attr_writer   ) método acessor

--- metodo_acessor.rb
+++ metodo_acessor.rb
@@ -1,4 +1,4 @@
-
+=begin
 class CSWebsite
   def initialize(website)
     @website = website
@@ -7,4 +7,20 @@ class CSWebsite
 end
 gfg = CSWebsite.new"www.geeksforgeeks.com.br"
 p gfg.website
+=end
+p "*****************************************************************"
+
+class Site
+  def initialize(site2)
+    @site2 = site2
+  end
+  attr_reader :site2
+
+  attr_writer :site2
+end
+gfg2 = Site.new "www.praticandoruby.com.br"
+puts  gfg2.site2
+gfg2.site2 ="www.aprendendendoruby.com.br"
+puts  gfg2.site2
+
 
