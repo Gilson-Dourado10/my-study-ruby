473e00c61a5f1ae1a5420b06dfe08674c9d1beda
gilson dourado <gilsondourado10@gmail.com>
2022-09-22 23:59:42 -0300

    feat:(classes )herança

--- heranca.rb
+++ heranca.rb
@@ -1,3 +1,4 @@
+=begin
 class Pai
   attr_accessor :nome , :idade
 
@@ -22,6 +23,31 @@ filhinha.idade = 18
 puts "meu nome é  " + filhinha.nome
 puts "eu fiz  ", + filhinha.idade
 puts filhinha.falar("hello friends")
+=end
+class Conta
+  def initialize(nome)
+
+  end
+    attr_accessor :endereco,:cep
+ def  depositar(valor)
+ end
+
+ def sacar
+   valor  = 100
+      if valor > saldo
+        puts  "saldo insuficiente para sacar"
+     end
+ end
+end
+class Conta_Corrente
+end
+c = Conta.new"Gilson Dourado"
+c.depositar "100"
+p c.endereco = "rua maria joana n° 300"
+p c.cep = " 09300-333"
+p c.sacar
+
+p"+++++++++++++++++++++++++++++++++++++"
 
 
 
