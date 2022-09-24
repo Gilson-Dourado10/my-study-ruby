e1ee1083e44bbae2210c6c34f92927b51f9805f1
gilson dourado <gilsondourado10@gmail.com>
2022-09-24 00:00:03 -0300

    feat:(classes )herança

--- heranca.rb
+++ heranca.rb
@@ -1,31 +1,31 @@
-class Pessoa
+class CONTA
   attr_accessor :nome, :email
 end
 
-class PessoFisica < Pessoa
+class ContaPoupança < CONTA
   attr_accessor :cpf
   def falar(texto)
     texto
   end
 end
 
-class PessoaJuridica < Pessoa
+class ContaCorrente < CONTA
   attr_accessor :cnpj
   def pagar_funcioario
     puts "pagando funcionário"
   end
 end
-p1= Pessoa.new
+p1= CONTA.new
 p p1.nome = "pedro vasconcelos"
 p p1.email = "pedro@pedro@hotmail.com"
 p"¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨"
-p2 =PessoFisica.new
+p2 =ContaPoupança.new
 p p2.nome = " pedro vasconcelos"
 p p2.email = "pedro@pedro@hotmail.com"
 p p2.cpf = "030.654.666-25"
 p p2.falar"hello friends"
 p"****************************************"
-p3 =PessoaJuridica.new
+p3 =ContaCorrente.new
 p p3.nome = " Sandro vasconcelos"
 p p3.email = "sandro@hotmail.com"
 p p3.cnpj = "336.6233.0001-36"
