43fb2e7e7111efc0004ac43cf8a5f13e2567b933
gilson dourado <gilsondourado10@gmail.com>
2022-09-24 23:26:33 -0300

    feat:(SELF ) método de CLASSE

--- self.rb
+++ self.rb
@@ -1,17 +1,10 @@
-class TesteSelf
-  def ola
-    puts "esse é meu self: #{self }"
+class Pessoa
+  def falar                        # MÉTODO DE INSTÂNCIA, PRECISA INSTANCIAR
+    "APRENDENDO MÉTODO SELF COM RUBY"
+  end
+  def self.cumprimentar(texto)    #  MÉTODO DE CLASSE, NÃO PRECISA INSTANCIAR
+      "#{texto}"
   end
 end
-p"*********************************"
-
-class Teste1
-  def meu_self
-    puts "esse é o meu self: #{self }"
-  end
-end
-t = Teste1.new
-t.meu_self
-
-teste = TesteSelf.new
-teste.ola
+          pessoa = Pessoa.new
+  puts  pessoa.falar
