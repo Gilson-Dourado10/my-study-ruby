2a67518f44be99059c560b4088b9931e2651f664
gilson dourado <gilsondourado10@gmail.com>
2022-09-25 23:36:44 -0300

    feat:(overring )sobrescrita de método

--- overring.rb
+++ overring.rb
@@ -1,17 +1,15 @@
-
-class Calculadora
-  def somar(n1,n2)
-    n1 +n2
+class A
+  def  a
+    puts "geeks"
   end
 end
 
- class CalculadorarFashion < Calculadora
-    def somar(n1,n2)
-      "a soma eh:  #{n1 +n2}  "
-    end
+class B < A
+  def a
+    puts "Benvindo ao geeks for geeks"
   end
-c= Calculadora.new
-puts c.somar(2,3)
+end
 
-c= CalculadorarFashion.new
-puts c.somar(2,3)
+b = B.new
+puts b.a
+
