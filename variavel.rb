199b8dce0790b0f7cafcd0e8f9c2e51632ab6e55
gilson dourado <gilsondourado10@gmail.com>
2024-04-06 06:01:01 -0300

    feat:(metodo ) método de classe

--- variavel.rb
+++ variavel.rb
@@ -1,3 +1,6 @@
+=begin
+    
+end
 def var_local
     @nome = "Pedro Sampaio"
     @idade = 38
@@ -7,3 +10,16 @@ end
 
 var_local
 puts "fora da função: #{@nome} e tem #{@idade} de idade"
+=end
+
+class Calculadora
+    def soma(a,b)
+        return a + b      
+    end
+
+    def self.subtracao(a,b)
+        return a - b
+    end
+end    
+puts Calculadora.subtracao(5,10) 
+puts Calculadora.soma(5,10)
