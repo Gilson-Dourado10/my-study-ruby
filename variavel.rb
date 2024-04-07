94c779423c1ef6635f847e2ec046f414f392db9a
gilson dourado <gilsondourado10@gmail.com>
2024-04-07 06:24:47 -0300

    feat:(metodo ) método initialize

--- variavel.rb
+++ variavel.rb
@@ -12,6 +12,7 @@ var_local
 puts "fora da função: #{@nome} e tem #{@idade} de idade"
 =end
 
+=begin
 class Calculadora
     def soma(a,b)
         return a + b      
@@ -24,3 +25,20 @@ end
 puts Calculadora.subtracao(5,10) 
 calculadora  = Calculadora.new
 puts calculadora.soma(5,10)
+
+=end
+
+class Pessoa
+    def initialize(nome, idade)
+      @nome = nome
+      @idade = idade
+    end
+    
+    def apresentar
+      puts "Olá, meu nome é #{@nome} e tenho #{@idade} anos."
+    end
+  end
+  
+  pessoa = Pessoa.new("Ana", 25)
+  pessoa.apresentar
+  
