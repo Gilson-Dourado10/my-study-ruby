67d4b4e9a4c3a8d1a4a75868d16f345e471c2c2c
gilson dourado <gilsondourado10@gmail.com>
2024-05-07 23:37:29 -0300

    feat:(array) com hash

--- variavel.rb
+++ variavel.rb
@@ -188,9 +188,28 @@ puts nomes[2]
 puts nomes[3] 
 =end
 
+=begin
 1.times do
   puts "JESUS SALVA"
   puts "É O UNICO SALVADOR"
+end
+=end
+
+colorArray = []  #  o mesmo que Array.new
+colorHash  = {}  #  o mesmo que Hash.new
+
+colorArray[0]         = 'vermelho'
+colorArray[1]         = 'verde'
+colorArray[2]         = 'azul'
+colorHash['strings']  = 'vermelho'
+colorHash['numbers']  = 'verde'
+colorHash['keywords'] = 'azul'
+
+colorArray.each do |color|
+  puts color
+end
+colorHash.each do |codeType, color|
+  puts codeType + ':  ' + color
 end
 
 
